214f1fbfa3a56ca70ace1bfcf33dba6af6bf0875
Ming Bian <127263871+Ming-B@users.noreply.github.com>
2025-03-04 22:20:00 -0500

    working code

--- NASANetworking/ComicView.swift
+++ NASANetworking/ComicView.swift
@@ -9,35 +9,33 @@ import SwiftUI
 
 
 struct NasaImage: Codable {
-    
     var date: String
     var explanation: String
     var title: String
     var url: String
-    var hdurl: String
+    var hdurl: String?
+    //var media_type: String
     
 }
 
 
 //image model for a nasa image of the day
 //@params: array of images(to contain the past few days)
-//array of thumbnailURLS to display on the main scroll list
-class NasaImageModel: ObservableObject {
-    @Published var pictures: [NasaImage] = []
-    @Published var thumbnailURLs: [URL] = []
+@Observable
+class NasaImageModel {
     
+    //ask if i even need the nasamodel, or can i get away with just using the struct and instantiating it
+    
+    var pictures: [NasaImage] = []
     //async function that awaits get picturs
     //refrehes the screen to fetch the new pictures of the day for the past 5 days
     func refresh() async {
         
-        let pastDates = getPastDates(count: 5)
+        let pastDates = getPastFiveDays()
         
         for date in pastDates {
             if let picture = await getPicture(for: date) {
                 self.pictures.append(picture)
-                if let url = URL(string: picture.url) {
-                    self.thumbnailURLs.append(url)
-                }
             }
         }
         
@@ -47,6 +45,7 @@ class NasaImageModel: ObservableObject {
     //async function that returns the picture of the day
     private func getPicture(for date: String) async -> NasaImage? {
         let session = URLSession(configuration: .default)
+        
         
         let key = "dOR3ZJ2YwfP4FgLBbajf6R7eazZY3Wz7wvUi7S6A"
         
@@ -68,13 +67,14 @@ class NasaImageModel: ObservableObject {
     }
     
     
-    private func getPastDates(count: Int) -> [String] {
+    private func getPastFiveDays() -> [String] {
         let calendar = Calendar.current
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         
         var pastDates: [String] = []
-        for i in 0...count {
+        
+        for i in 0...5 {
             if let date = calendar.date(byAdding: .day, value: -i, to: Date()) {
                 pastDates.append(dateFormatter.string(from: date))
                 
@@ -84,22 +84,39 @@ class NasaImageModel: ObservableObject {
         return pastDates
     }
     
+    
+    //function to calculate the day that was 5 days before current
+    private func getStartDate(date: Date) -> String {
+        let calendar = Calendar.current
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        if let newDate = calendar.date(byAdding: .day, value: -5, to: date) {
+            return dateFormatter.string(from: newDate)
+        }
+        
+        return dateFormatter.string(from: date)
+        
+        
+    }
+    
+    
 }
 
 
 //detail view for whenever user taps on a thumbnail in the main screen
 //instance of NasaImage is used. Simple scroll view that has the article's title, image and explanation
 struct DetailView: View {
-    let picture: NasaImage
+    let apod: NasaImage
     
     var body: some View {
         ScrollView {
             VStack {
-                Text(picture.title)
+                Text(apod.title)
                     .font(.title)
                     .padding()
                 
-                AsyncImage(url: URL(string: picture.url)) { image in
+                AsyncImage(url: URL(string: apod.url)) { image in
                     image
                         .resizable()
                         .aspectRatio(contentMode: .fit)
@@ -108,7 +125,8 @@ struct DetailView: View {
                     ProgressView()
                 }
                 
-                Text(picture.explanation)
+                
+                Text(apod.explanation)
                     .font(.body)
                     .padding()
                 
@@ -123,7 +141,7 @@ struct DetailView: View {
 //list of past 5 days worth of APODS, structured in a way such that you can navigate to a more detailed view
 //click on thumbnail to go into detailed view
 struct ComicView: View {
-    @StateObject var nasaModel = NasaImageModel()
+    @State var nasaModel = NasaImageModel()
     @State var fetchingImage = false
     
     func loadImage() {
@@ -141,49 +159,42 @@ struct ComicView: View {
                     .font(.headline)
                     .padding()
                 
-                List {
-                    ForEach(Array(zip(nasaModel.pictures, nasaModel.thumbnailURLs)), id: \.0.date) { picture, thumbnailURL in
-                        NavigationLink {
-                            DetailView(picture: picture)
-                        } label: {
-                            HStack {
-                                AsyncImage(url: thumbnailURL) { image in
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 100, height: 100)
-                                        .cornerRadius(10)
-                                } placeholder: {
-                                    ProgressView()
-                                }
-                                
-                                Text(picture.title)
-                                    .font(.headline)
+                List(nasaModel.pictures, id: \.date) { picture in
+                    NavigationLink{
+                        DetailView(apod: picture)
+                    } label: {
+                        HStack {
+                            AsyncImage(url: URL(string: picture.url)) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100, height: 100)
+                                    .cornerRadius(10)
+                            } placeholder: {
+                                ProgressView()
                             }
+                            
+                            Text(picture.title)
+                                .font(.headline)
                         }
                     }
+                    
                 }
                 .listStyle(.plain)
+                
                 
                 Button {
                     loadImage()
                 } label: {
                     Text("Get APOD")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.white)
-                        .frame(width: 360, height: 44)
-                        .background(Color(.systemBlue))
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .padding(.vertical)
                 }
                 .disabled(fetchingImage)
             }
-            .onAppear() {
-                loadImage()
-            }
-            .padding()
         }
+        .onAppear() {
+            loadImage()
+        }
+        .padding()
     }
     
 }
